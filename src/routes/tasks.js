9a88b4c47e61d5b93270ed8c0a1969a1ab35159e
Mariano Troncoso <petotronco@gmail.com>
2020-11-21 11:19:19 -0300

    delete task method

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -17,21 +17,34 @@ router.post('/', async (req, res) => {
     });
 });
 
-// NO uso el id de mongo
-router.put('/:id', async (req, res) => {
-    
-    console.log(req.params);
+// // NO uso el id de mongo (ORIGINAL)
+// router.put('/:id', async (req, res) => {
 
-    // db.inventory.find( { status: "A" }, { item: 1, status: 1 } )
-
-    // obtengo el documento que quiero actualizar mediante el "id" (no "_id") 
-    const oldTask = await Task.find({"id": req.params.id})
-    console.log(oldTask[0]._id) 
+//     // obtengo el documento que quiero actualizar mediante el "id" (no "_id") 
+//     const oldTask = await Task.find({"id": req.params.id})
+//     console.log(oldTask[0]._id) 
         
-    await Task.findByIdAndUpdate(oldTask[0]._id, req.body)
+//     await Task.findByIdAndUpdate(oldTask[0]._id, req.body)
+
+//     res.json({
+//         status: "Task updated"
+//     });
+// });
+
+// NUEVO (ahora con el id de mongo)
+router.put('/:id', async (req, res) => {
+        
+    await Task.findByIdAndUpdate(req.params.id, req.body)
 
     res.json({
         status: "Task updated"
+    });
+});
+
+router.delete('/:id', async(req, res) => {
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({
+        status: "Task deleted"
     });
 })
 
