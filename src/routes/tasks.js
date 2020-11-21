399ba2f000529075d348e9917aabf5b6c42e89c0
Mariano Troncoso <petotronco@gmail.com>
2020-11-21 11:12:44 -0300

    put task method

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -17,4 +17,22 @@ router.post('/', async (req, res) => {
     });
 });
 
+// NO uso el id de mongo
+router.put('/:id', async (req, res) => {
+    
+    console.log(req.params);
+
+    // db.inventory.find( { status: "A" }, { item: 1, status: 1 } )
+
+    // obtengo el documento que quiero actualizar mediante el "id" (no "_id") 
+    const oldTask = await Task.find({"id": req.params.id})
+    console.log(oldTask[0]._id) 
+        
+    await Task.findByIdAndUpdate(oldTask[0]._id, req.body)
+
+    res.json({
+        status: "Task updated"
+    });
+})
+
 module.exports = router
